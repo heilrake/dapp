4c9089e3987c98a17e794f5eb56b4bc89c66d046
pumb3r <mishakvochko@ukr.net>
2022-10-07 16:04:33 +0300

    bugfixs market card

--- components/ui/course/index.js
+++ components/ui/course/index.js
@@ -3,3 +3,4 @@ export { default as CourseHero } from "./hero";
 export { default as Curriculum } from "./curriculum";
 export { default as Keypoints } from "./keypoints";
 export { default as CourseCard } from "./card";
+export { default as OwnedCourseCard } from "./card/OwnedCourseCard";
