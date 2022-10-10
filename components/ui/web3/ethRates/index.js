a0d475f8f634fc67e75f9e61860e293d92f70b8c
pumb3r <mishakvochko@ukr.net>
2022-10-10 17:04:57 +0300

    loader

--- components/ui/web3/ethRates/index.js
+++ components/ui/web3/ethRates/index.js
@@ -1,4 +1,5 @@
-import { useEthPrice } from "@components/hooks/web3/useEhtPrice";
+import { useEthPrice, COURSE_PRICE } from "@components/hooks/web3/useEhtPrice";
+import { Loader } from "@components/ui/common";
 import Image from "next/image";
 
 export default function EthRates() {
@@ -10,15 +11,22 @@ export default function EthRates() {
         <div className="p-10 border drop-shadow rounded-md">
 
           <div className="flex items-center">
-            <Image
-              layout="fixed"
-              height="35"
-              width="35"
-              src="/small-eth.webp"
-            />
-            <span className="text-2xl font-bold">
-              = {eth.data}$
-            </span>
+            {eth.data ?
+              <>
+                <Image
+                  layout="fixed"
+                  height="35"
+                  width="35"
+                  src="/small-eth.webp"
+                />
+                <span className="text-2xl font-bold">
+                  = {eth.data}$
+                </span>
+              </> :
+              <div className="w-full flex justify-center">
+                <Loader size="md" />
+              </div>
+            }
           </div>
           <p className="text-xl text-gray-500">Current eth Price</p>
         </div>
@@ -26,18 +34,25 @@ export default function EthRates() {
       <div className="flex flex-1 items-stretch text-center">
         <div className="p-10 border drop-shadow rounded-md">
           <div className="flex items-center">
-            <span className="text-2xl font-bold">
-              {eth.perItem}
-            </span>
-            <Image
-              layout="fixed"
-              height="35"
-              width="35"
-              src="/small-eth.webp"
-            />
-            <span className="text-2xl font-bold">
-              = 15$
-            </span>
+            {eth.data ?
+              <>
+                <span className="text-2xl font-bold">
+                  {eth.perItem}
+                </span>
+                <Image
+                  layout="fixed"
+                  height="35"
+                  width="35"
+                  src="/small-eth.webp"
+                />
+                <span className="text-2xl font-bold">
+                  = {COURSE_PRICE}$
+                </span>
+              </> :
+              <div className="w-full flex justify-center">
+                <Loader size="md" />
+              </div>
+            }
           </div>
           <p className="text-xl text-gray-500">Price per course</p>
         </div>
