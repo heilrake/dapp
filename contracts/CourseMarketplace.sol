129d986afb05ea8ceacdc3065bf990179cda5d0b
pumb3r <mishakvochko@ukr.net>
2022-10-11 16:33:33 +0300

    Store Course

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract CourseMarketplace {
 
-  enum State {
+  enum   State {
     Purchased,
     Activated,
     Deactivated
@@ -15,5 +15,27 @@ contract CourseMarketplace {
     bytes32 proof; // 32
     address owner; // 20
     State state; // 1
+  }
+    // mapping of courseHash to Course data
+  mapping(bytes32 => Course) private ownedCourses;
+
+  // mapping of courseID to courseHash
+  mapping(uint => bytes32) private ownedCourseHash;
+
+  // number of all courses + id of the course
+  uint private totalOwnedCourses;
+
+  function purchaseCourse(bytes16 courseId, bytes32 proof) external payable  {
+    bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+    uint id = totalOwnedCourses++;
+
+    ownedCourseHash[id] = courseHash;
+    ownedCourses[courseHash] = Course({
+      id: id,
+      price: msg.value,
+      proof: proof,
+      owner: msg.sender,
+      state: State.Purchased
+    });
   }
 }
