e266021096ae35a9cd29edf963c1eb4ff4516d75
pumb3r <mishakvochko@ukr.net>
2022-10-17 11:37:32 +0300

    display owned courses

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -2,39 +2,42 @@ import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
-import { useAccount,useOwnedCourses } from "@components/hooks/web3";
+import { useAccount, useOwnedCourses } from "@components/hooks/web3";
 import { getAllCourses } from "@content/courses/fetcher";
-export default function OwnedCourses({courses}) {
-  const { account } = useAccount();
-  const { ownedCourses } = useOwnedCourses(courses, account.data);
- 
+export default function OwnedCourses({ courses }) {
+  const { account } = useAccount()
+  const { ownedCourses } = useOwnedCourses(courses, account.data)
+
   return (
     <>
-      { JSON.stringify(ownedCourses.data) }
       <div className="py-4">
         <MarketHeader />
       </div>
       <section className="grid grid-cols-1">
-        <OwnedCourseCard>
-          <Message>
-            My custom message!
-          </Message>
-          <Button>
-            Watch the course
-          </Button>
-        </OwnedCourseCard>
+        {ownedCourses.data?.map(course =>
+          <OwnedCourseCard
+            key={course.id}
+            course={course}
+          >
+            {/* <Message>
+              My custom message!
+            </Message> */}
+            <Button>
+              Watch the course
+            </Button>
+          </OwnedCourseCard>
+        )}
       </section>
     </>
   )
-};
+}
 
 export function getStaticProps() {
-  const { data } = getAllCourses();
+  const { data } = getAllCourses()
   return {
     props: {
       courses: data
     }
   }
-};
-
+}
 OwnedCourses.Layout = BaseLayout;
