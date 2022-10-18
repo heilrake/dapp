2cadf2556eb4864c992a852e6c74d8d3ada1dc19
pumb3r <mishakvochko@ukr.net>
2022-10-18 16:10:19 +0300

    one time event

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -21,15 +21,11 @@ export const handler = (web3, provider) => () => {
   );
 
   useEffect(() => {
-    console.log("SUBSCRIBING TO EVENT");
-    provider &&
-      provider.on("accountsChanged",
-        accounts => {
-          console.log("ON ACCOUNT DATA");
-          mutate(accounts[0] ?? null)
-        }
-      )
-    console.log(provider);
+    const mutator = account = mutate(account[0] ?? null);
+    provider?.on("accountsChanged", mutator);
+    return () => {
+      provider?.removeListener("accountsChanged", mutator)
+    }
   }, [provider]);
 
   return {
