f4ddc3ff2e3485a147adb6d5c63c1ac1ea4193c3
pumb3r <mishakvochko@ukr.net>
2022-10-20 10:35:35 +0300

    fetch managed courses

--- components/providers/web3/hooks/useManagedCourses.js
+++ components/providers/web3/hooks/useManagedCourses.js
@@ -6,11 +6,21 @@ export const handler = (web3, contract) => account => {
   const swrRes = useSWR(() =>
     (web3 && contract && account) ? `web3/managedCourses/${account}` : null,
     async () => {
-      const courses = [1, 2, 3, 4]
+      const courses = [];
+      const courseCount = await contract.methods.getCourseIndex().call();
 
-      return courses
+      for (let i = Number(courseCount) - 1; i >= 0; i--) {
+        const courseHash = await contract.methods.getCourseHashAtIndex(i).call();
+        const course = await contract.methods.getCourseByHash(courseHash).call();
+
+        if (course) {
+          const normalized = normalizeOwnedCourse(web3)({ hash: courseHash }, course)
+          course.push(normalized)
+        }
+      }
+
+      return courses;
     }
   )
-
-  return swrRes
-}
+  return swrRes;
+};
