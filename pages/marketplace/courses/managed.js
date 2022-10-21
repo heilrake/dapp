c94e58535a187ea5aab42ac0edbb51e085e52936
pumb3r <mishakvochko@ukr.net>
2022-10-21 11:45:14 +0300

    verification input

--- pages/marketplace/courses/managed.js
+++ pages/marketplace/courses/managed.js
@@ -5,8 +5,32 @@ import { CourseFilter, ManagedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 import { useState } from "react";
-export default function ManagedCourses() {
+
+
+const VerificationInput = ({ onVerify }) => {
   const [email, setEmail] = useState("");
+
+  return (
+    <div className="flex mr-2 relative rounded-md">
+      <input
+        value={email}
+        onChange={({ target: { value } }) => setEmail(value)}
+        type="text"
+        name="account"
+        id="account"
+        className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
+        placeholder="0x2341ab..." />
+      <Button
+        onClick={() => {
+          onVerify(email)
+        }}
+      >
+        Verify
+      </Button>
+    </div>
+  )
+};
+export default function ManagedCourses() {
   const { account } = useAccount();
   const { managedCourses } = useManagedCourses(account.data);
   const { web3 } = useWeb3();
@@ -38,24 +62,14 @@ export default function ManagedCourses() {
             key={course.ownedCourseId}
             course={course}
           >
-            <div className="flex mr-2 relative rounded-md">
-              <input
-                value={email}
-                onChange={({ target: { value } }) => setEmail(value)}
-                type="text"
-                name="account"
-                id="account"
-                className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
-                placeholder="0x2341ab..." />
-              <Button onClick={() => {
+            <VerificationInput
+              onVerify={email => {
                 verifyCourse(email, {
                   hash: course.hash,
                   proof: course.proof
                 })
-              }}>
-                Verify
-              </Button>
-            </div>
+              }}
+            />
             {proofedOwnership[course.hash] &&
               <div className="mt-2">
                 <Message>
